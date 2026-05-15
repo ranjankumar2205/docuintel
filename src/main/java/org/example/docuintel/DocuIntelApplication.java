5f1538257a7541772877519c5fb380b074cecf0a
Ranjan <ranjandm82@gmail.com>
2026-05-15 23:11:03 +0530

    chore(init): initial project setup

---
+++ src/main/java/org/example/docuintel/DocuIntelApplication.java
@@ -0,0 +1,13 @@
+package org.example.docuintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuIntelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocuIntelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/docuintel/DocuIntelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.docuintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuIntelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
